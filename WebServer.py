2fa81d657d9fe6329110458ddb0f3f6f12fc07c9
Ennil23541 <Cherrypcchan@gmail.com>
2025-10-27 16:45:35 -0700

    Finished 404 and a bit of 200 in serve_html_file

--- WebServer.py
+++ WebServer.py
@@ -1,10 +1,13 @@
 from socket import *
+import os
 
 serverPort = 12000
 
 serverSocket = socket(AF_INET,SOCK_STREAM)
 
 serverSocket.bind(('',serverPort))
+
+serverRoot = "."
 
 serverSocket.listen(1)
 print ('The server is ready to receive')
@@ -15,8 +18,18 @@ print ('The server is ready to receive')
 
 
 
-
-
+#404 and 200
+def serve_html_file(file_name):
+    file_path = os.path.join(serverRoot, file_name)
+    try:
+        with open(file_path, 'rb') as f:
+            content = f.read()
+        headers = b"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n"
+        return headers + content
+    except FileNotFoundError:
+        error_content = b"<h1>404 Not Found</h1><p>This is Yan Ting and we cannot find the requested file.</p>"
+        headers = b"HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n"
+        return headers + error_content
 
 
 
@@ -34,7 +47,18 @@ while True: # Loop forever
 
 
      #404
-     
+     #if not sentence:
+     #     break
+
+     print(sentence) #see actual http request with header
+
+     if sentence.startswith("GET"):
+          path = (sentence.split())[1][1:]
+          print(path)
+          response = serve_html_file(path)
+          #if os.path.exists(path):
+          
+               
 
 
 
@@ -43,8 +67,7 @@ while True: # Loop forever
 
      
      # Send the reply
-     capitalizedSentence = sentence.upper()
-     connectionSocket.send(capitalizedSentence.encode())
+     connectionSocket.send(response)
      
      # Close connection to client (but not welcoming socket)
      connectionSocket.close()
